1ccb24e78fc3fd44b5cabae5fffe81cdb1914bb3
xudecheng <dxu104@syr.edu>
2023-02-15 22:01:16 -0800

    HW3main

--- main.tf
+++ main.tf
@@ -21,7 +21,6 @@ module "vpc" {
 
 
 #   source = "./Module/aws_vpcs"
-
 #   profile             = var.profile
 #   public_subnets_num=var.private_subnets_num
 #   private_subnets_num = var.private_subnets_num
@@ -60,16 +59,19 @@ module "vpc" {
 #   region = "us-east-2"
 #   availability_zones = ["us-east-2a", "us-east-2b", "us-east-2c"]
 
-#   profile             = var.profile
-#   public_subnets_num=var.private_subnets_num
-#   private_subnets_num = var.private_subnets_num
-#   vpc_name = "us-west-2-vpc-2"
-#   vpc-cidr = "10.1.0.0/16"
-#   ami_id = var.ami_id
-#   region = var.region
-
-#   availability_zones = ["${var.region}a", "${var.region}b", "${var.region}c"]
-
-
 # }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
